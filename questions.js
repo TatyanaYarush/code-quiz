a9095eaccbddaa845b400b0372ddbbe7b87d2496
Tanya Y <brulek7@gmail.com>
2021-03-11 18:11:55 -0500

    created questions.js file and add new code

---
+++ questions.js
@@ -0,0 +1,72 @@
+var question = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: [
+            "<script>",
+            "<js>",
+            "<javascript>",
+            "<JS>"
+        ],
+        answer: "<script>"
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        choices: [
+            "<script name='./xxx.js'>",
+            "<script href='./xxx.js'>",
+            "<script scr='./xxx.js'>",
+            "<script ='./xxx.js'>"
+        ],
+        answer: "<script scr='./xxx.js'>"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        choices: [
+            "function = myFunction()",
+            "function:myFunction(",
+            "function myFunction()",
+            "myFunction()"
+        ],
+        answer: "function myFunction()"
+    },
+    {
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        choices: [
+            "if (i != 5)  ",
+            "if (i <> 5)",
+            "if i =! 5 then",
+            "if i <> 5"
+        ],
+        answer: "if (i != 5)  "
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        choices: [
+            "var colors = (1:'red', 2:'green', 3:'blue')",
+            "var colors = ['red', 'green', 'blue']",
+            "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
+            "var colors = 'red', 'green', 'blue'"
+        ],
+        answer: "var colors = ['red', 'green', 'blue']"
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        choices: [
+            "//This is a comment",
+            "'This is a comment",
+            "<!--This is a comment-->",
+            "<This is a comment//>"
+        ],
+        answer: "//This is a comment"
+    },
+    {
+        question: "What is the data type of variables in JavaScript?",
+        choices: [
+            "Object data types",
+            "Function data type",
+            "None of the above",
+            "All of the above"
+        ],
+        answer: "Object data types"
+    }
+];
